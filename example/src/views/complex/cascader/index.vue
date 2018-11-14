02d93247e9559490cacc5ec6ef06cd71dace5669
ektx <530675800@qq.com>
2018-11-14 11:27:23 +0800

    cascader集联功能

--- example/src/views/complex/cascader/index.vue
+++ example/src/views/complex/cascader/index.vue
@@ -1,7 +1,21 @@
 <template>
     <div>
         <h2>集联组件</h2>
-		<router-view/>
+        
+        <h3>基础示例</h3>
+		<p @click="show = !show">目前选择为: {{value}}</p>
+
+        <v5-cascader :show.sync="show" :data="data" v-model="value" deep/>
+
+        <h3>可以选择父级</h3>
+		<p @click="show2 = !show2">目前选择为: {{value}}</p>
+
+        <v5-cascader :show.sync="show2" :data="data" v-model="value"/>
+        
+        <h3>自定义选择条件</h3>
+		<p @click="show3 = !show3">目前选择为: {{value2}}</p>
+
+        <v5-cascader :show.sync="show3" :data="data2" v-model="value2" :filter="filter"/>
     </div>
 </template>
 
@@ -10,14 +24,21 @@ export default {
     name: 'cascader-demo',
     data () {
         return {
+            show: false,
+            show2: false,
+            show3: false,
+            value2: [],
+            value: ['jiangsu', 'nanjing', 'fuzimiao'],
             data: [
                 {
                     value: 'beijing',
                     label: '北京',
+                    subTitle: '中国·首都',
                     children: [
                         {
                             value: 'gugong',
-                            label: '故宫'
+                            label: '故宫',
+                            disabled: true
                         },
                         {
                             value: 'tiantan',
@@ -32,10 +53,12 @@ export default {
                 {
                     value: 'jiangsu',
                     label: '江苏',
+                    disabled: true,
                     children: [
                         {
                             value: 'nanjing',
                             label: '南京',
+                            subTitle: '省会',
                             children: [
                                 {
                                     value: 'fuzimiao',
@@ -57,9 +80,101 @@ export default {
                                 }
                             ]
                         }
+                    ],
+                },
+                {
+                    value: 'zhejiang',
+                    label: '浙江',
+                    children: [
+                        {
+                            value: 'hangzhou',
+                            label: '杭州',
+                            subTitle: '省会',
+                            children: [
+                                {
+                                    value: 'xihu',
+                                    label: '西湖'
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    value: 'taiwang',
+                    label: '台湾',
+                    disabled: true,
+                    children: [
+                        {
+                            value: 'taibei',
+                            label: '台北'
+                        }
+                    ]
+                },
+            ],
+            data2: [
+                {
+                    value: 1000,
+                    label: 1000,
+                    children: [
+                        {
+                            value: 1100,
+                            label: 1100
+                        },
+                        {
+                            value: 1200,
+                            label: 1200,
+                            children: [
+                                {
+                                    value: 1210,
+                                    label: 1210
+                                },
+                                {
+                                    value: 1220,
+                                    label: 1220
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    value: 2000,
+                    label: 2000,
+                    children: [
+                        {
+                            value: 2100,
+                            label: 2100
+                        },
+                        {
+                            value: 2200,
+                            label: 2200
+                        }
+                    ]
+                },
+                {
+                    value: 3000,
+                    label: 3000,
+                    children: [
+                        {
+                            value: 3100,
+                            label: 3100
+                        },
+                        {
+                            value: 3100,
+                            label: 3100
+                        }
                     ]
                 }
             ]
+        }
+    },
+    methods: {
+        filter (item, deep) {
+            console.log(item, deep)
+
+            // 如果选择的项目 值大于2000的可以选择确认
+            deep = item.value > 2000
+
+            return deep
         }
     }
 }
--- src/components/cascader/cascader.vue
+++ src/components/cascader/cascader.vue
@@ -1,15 +1,183 @@
 <template>
-    <div class="v5-cascader-mod">
+    <v5-layer class="v5-cascader-mod" :show="show" position="right">
+        <div class="v5-cascader-inner-mod">
+            <header class="v5-cascader-header">
+                <span 
+                    v-for="(nav, i) in result" 
+                    :key="nav.value" 
+                    :class="[{child: nav.children}]"
+                    @click="change(nav, i)"
+                >{{nav.label}}</span>
 
-    </div>
+                <i v-show="result.length" class="close-btn" @click="clear">&#xe611;</i>
+            </header>
+            <div class="current-list-box">
+                <v5-cell 
+                    v-for="(item, index) in current" 
+                    :key="index" 
+                    :title="item.label"
+                    :subTitle="item.subTitle"
+                    :icon="item.children ? 'right' : ''"
+                    @click="update(item)"
+                    :disabled="item.disabled"
+                />
+            </div>
+            <footer>
+                <button @click="cancel">取消</button>
+                <button :disabled="!confirm" @click="send">确认</button>
+            </footer>
+        </div>
+    </v5-layer>
 </template>
 
 <script>
 export default {
     name: 'v5-cascader',
+    props: {
+        // 选择值
+        value: {
+            type: Array,
+            default: () => []
+        },
+        // 选择内容
+        data: {
+            type: Array,
+            default: () => []
+        },
+        // 控制弹层显隐
+        show: {
+            type: Boolean,
+            default: false
+        },
+        // 只能选择到最深才可以返回值
+        deep: {
+            type: Boolean,
+            default: false
+        },
+        /* 接受用户对确认按钮的控制
+         * @return {Boolean}
+         */
+        filter: Function
+    },
     data () {
         return {
-            
+            result: [],
+            current: [],
+            list: {},
+            // 按钮点击控制器
+            confirm: true
+        }
+    },
+    watch: {
+        current (val) {
+            // 如果存在 deep
+            if (this.deep) {
+                // 只有在当前列表没有选择时，才可以返回值
+                this.confirm = val.length <= 0
+            }
+        },
+        show (val) {
+            if (val) {
+                // 清空老数据 
+                this.current = []
+                this.result = []
+
+                this.loop(this.data)
+                this.getLabel()
+        
+                // 处理默认值回填
+                if (this.result.length) {
+                    let child = this.result.slice(-1)[0].children
+                    this.current = child ? child : []
+                } else {
+                    // 没有默认值就使用 data
+                    this.current = this.data
+                }
+        
+                if (this.deep) {
+                    this.confirm = false
+                }
+            }
+        }
+    },
+    mounted () {
+    },
+    methods: {
+        /**
+         * 处理数据回填使用
+         * @param {Array} data 用户传入的数组
+         * @param {String} parent 父级，用于在处理回填时，对上级可以准确判断，防止级别错误 
+         */
+        loop (data, parent) {
+            data.forEach((item, i) => {
+                this.list[item.value] = item
+
+                if (parent) {
+                    this.list[item.value].parent = parent
+                }
+                
+                if (item.children) {
+                    this.loop(item.children, item.value)
+                }
+            })
+        },
+
+        getLabel () {
+            this.value.forEach((key, index) => {
+                let item = this.list[key]
+
+                if (!item) return
+
+                // 对父级判断
+                // 有父级的情况下，如果对应不上，则显示最近正确的层级
+                if (item.parent) {
+                    if (this.value[index -1] !== item.parent) {
+                        return
+                    }
+                }
+
+                this.result.push( item )
+            })
+        },
+
+        update (item) {
+            if (this.filter) {
+                // 接受确认按钮状态
+                this.confirm = this.filter(item, this.confirm)
+            } 
+
+            this.result.push(item)
+            this.current = item.children ? item.children : []
+        },
+
+        change (nav, i) {
+            if (this.filter) {
+                // 接受确认按钮状态
+                this.confirm = this.filter(nav, this.confirm)
+            } 
+
+            this.current = nav.children ? nav.children : []
+            this.result = this.result.slice(0, i+1)
+        },
+
+        clear () {
+            this.result = []
+            this.current = this.data
+        },
+
+        send () {
+            if (this.confirm) {
+                let data = this.result.map(val => {
+                    return val.value
+                })
+
+                this.$emit('input', data)
+                this.$emit('update:show', false)
+            }
+        },
+        
+        cancel () {
+            this.$emit('update:show', false)
         }
     }
 }
