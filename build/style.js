e8b0e10e93e6524622fd40a67ec33fa41798f5e7
ektx <530675800@qq.com>
2019-06-28 21:52:46 +0800

    更新样式打包功能

--- build/style.js
+++ build/style.js
@@ -1,22 +1,18 @@
-const gulp = require('gulp')
+const { src, dest } = require('gulp')
 const sass = require('gulp-sass')
 const autoprefixer = require('gulp-autoprefixer')
 const rename = require('gulp-rename')
 
 sass.compiler = require('node-sass')
 
-gulp.task('sass', function () {
-    return gulp.src('../src/styles/*.scss')
+exports.default = function () {
+    return src('../src/styles/*.scss')
         .pipe(sass({
             outputStyle: 'compressed'
         }).on('error', sass.logError))
         .pipe(autoprefixer({
-            browsers: ['last 2 versions', 'ie > 8']
+            overrideBrowserslist: ['last 2 versions', 'ie > 8']
         }))
         .pipe(rename('v5.css'))
-        .pipe(gulp.dest('../dist'))
-})
-
-// 使用 gulp.task('default') 定义默认任务
-// 在命令行使用 gulp 启动 sass 任务
-gulp.task('default', ['sass'])
+        .pipe(dest('../dist'))
+}
