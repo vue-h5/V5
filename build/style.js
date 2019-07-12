02212f765d6364bae756a96edfbcdacfbba57bde
wuming <530675800@qq.com>
2019-07-12 15:07:43 +0800

    :art: 优化样式生成方式

--- build/style.js
+++ build/style.js
@@ -2,13 +2,15 @@ const { src, dest } = require('gulp')
 const sass = require('gulp-sass')
 const autoprefixer = require('gulp-autoprefixer')
 const rename = require('gulp-rename')
+const importOnce = require('node-sass-import-once')
 
 sass.compiler = require('node-sass')
 
 exports.default = function () {
     return src('../src/styles/*.scss')
         .pipe(sass({
-            outputStyle: 'compressed'
+            outputStyle: 'compressed',
+            importer: importOnce
         }).on('error', sass.logError))
         .pipe(autoprefixer({
             overrideBrowserslist: ['last 2 versions', 'ie > 8']
